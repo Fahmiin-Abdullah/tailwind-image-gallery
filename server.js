ac67b46ce4d6198fea505e05236982cfa5e39d44
fahmiin <fahmiin@tyne-solutions.com>
2020-07-12 01:04:27 +0800

    Installed dotenv

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, 'build')
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.static(publicPath))
