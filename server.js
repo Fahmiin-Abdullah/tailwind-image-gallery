05e914bd28831bc43d23939de5af2db702c2665c
fahmiin <fahmiin@tyne-solutions.com>
2020-07-12 00:56:09 +0800

    Installed cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, 'build')
 
+app.use(cors())
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => res.sendFile(path.join(publicPath, 'index.html')))
